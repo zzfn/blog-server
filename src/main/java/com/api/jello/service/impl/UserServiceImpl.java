eb9f4f0145d84be14e13240f0af7294ffa37371c
nanaouyang <Gg3.1415926>
2020-08-09 21:55:57 +0800

    🚑

--- src/main/java/com/api/jello/service/impl/UserServiceImpl.java
+++ src/main/java/com/api/jello/service/impl/UserServiceImpl.java
@@ -1,13 +1,13 @@
 package com.api.jello.service.impl;
 
-import com.api.jello.entity.User;
 import com.api.jello.dao.UserDao;
+import com.api.jello.dao.UserRoleDao;
+import com.api.jello.entity.User;
 import com.api.jello.service.UserService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 
@@ -22,11 +22,13 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     @Autowired
     UserDao userDao;
+    @Autowired
+    UserRoleDao userRoleDao;
 
     @Override
     public UserDetails loadUserByUsername(String username) {
         User user = userDao.selectOne(new QueryWrapper<User>().eq("username", username));
-        log.info("用户{}", user);
+        user.setRoleList(userRoleDao.getRoles(user.getId()));
         return user;
     }
 }
