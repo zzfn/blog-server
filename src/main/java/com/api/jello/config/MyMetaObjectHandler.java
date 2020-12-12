4933a2fdac91c2192b6ad66ee72f60958f7ffc33
zzfn <chenc@fengyuntec.com>
2020-12-12 23:06:18 +0800

    feat: fix

--- src/main/java/com/api/jello/config/MyMetaObjectHandler.java
+++ src/main/java/com/api/jello/config/MyMetaObjectHandler.java
@@ -17,6 +17,7 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
         this.strictInsertFill(metaObject, "isDelete", Integer.class, 0);
     }
 
