03abf3e45242b4c0ba9bb4e885d49b1d1b5eef09
chenc <chenchen3@fosun.com>
2021-08-27 12:33:17 +0800

    feat: 邮件测试

--- src/main/java/com/zzf/controller/TraceController.java
+++ src/main/java/com/zzf/controller/TraceController.java
@@ -5,18 +5,33 @@ import com.zzf.component.IgnoreAuth;
 import com.zzf.entity.Trace;
 import com.zzf.util.HttpUtil;
 import com.zzf.util.ResultUtil;
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * TRACE(Trace)表控制层
@@ -33,7 +48,12 @@ public class TraceController {
      */
     @Resource
     private MongoTemplate mongoTemplate;
-
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Autowired
+    private Configuration configuration;
+    @Value("${spring.mail.username}")
+    private String from;
     @ApiOperation("ip查询")
     @GetMapping("ip")
     @IgnoreAuth
@@ -41,12 +61,36 @@ public class TraceController {
         return ResultUtil.success(HttpUtil.getIp());
     }
 
+    @ApiOperation("mail")
+    @GetMapping("mail")
+    @IgnoreAuth
+    public Object mail() throws MessagingException, IOException, TemplateException {
+        MimeMessage mimeMailMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMailMessage, true);
+        mimeMessageHelper.setFrom(from);
+        mimeMessageHelper.setTo("admin@annyyy.com");
+        mimeMessageHelper.setSubject("SpringBoot测试邮件发送");
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("title", "邮件标题");
+        map.put("content", "邮件正文");
+        String text = FreeMarkerTemplateUtils.processTemplateIntoString(configuration.getTemplate("mail.ftl"), map);
+        mimeMessageHelper.setText(text, true);
+
+        String url = "https://cdn.zzfzzf.com/1621500127578INeO4C.jpg";
+        URL imgUrl = new URL(url);
+        mimeMessageHelper.addAttachment("img.jpg", imgUrl::openStream);
+
+        javaMailSender.send(mimeMailMessage);
+        return ResultUtil.success(HttpUtil.getIp());
+    }
+
     @GetMapping("list")
     @IgnoreAuth
     public Object send() {
-        Query query=new Query();
+        Query query = new Query();
         query.with(Sort.by("time").descending());
-        return ResultUtil.success(mongoTemplate.find(query,Trace.class, "logs"));
+        return ResultUtil.success(mongoTemplate.find(query, Trace.class, "logs"));
     }
 
     @ApiOperation("服务器信息")
