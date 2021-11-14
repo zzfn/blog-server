f8414b748ea6cb72e356f39799ae81f185033ca4
cc <cc@zzfzzf.com>
2021-11-14 21:37:32 +0800

    feat: 搜索埋点

---
+++ src/main/java/com/zzf/vo/PerformanceVO.java
@@ -0,0 +1,14 @@
+package com.zzf.vo;
+
+import lombok.Data;
+
+/**
+ * @author cc
+ */
+@Data
+public class PerformanceVO {
+    private String id;
+    private Double min;
+    private Double avg;
+    private Double max;
+}
