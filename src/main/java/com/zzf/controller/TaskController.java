0aa393e315254af2f9a7accc9311f966c0f86714
chenc <44751363+zzfn@users.noreply.github.com>
2022-04-13 19:36:57 +0800

    feat: task

--- src/main/java/com/zzf/controller/TaskController.java
+++ src/main/java/com/zzf/controller/TaskController.java
@@ -2,16 +2,9 @@ package com.zzf.controller;
 
 import com.zzf.annotation.IgnoreAuth;
 import com.zzf.entity.Task;
-import com.zzf.mapper.TalkBotMapper;
-import com.zzf.mapper.TaskMapper;
 import com.zzf.service.TaskService;
 import com.zzf.util.ResultUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -32,7 +25,7 @@ public class TaskController {
 
     @PostMapping("save")
     @IgnoreAuth
-    public Object saveTask(Task task) {
+    public Object saveTask(@RequestBody Task task) {
         return ResultUtil.success(taskService.saveOrUpdate(task));
     }
 }
