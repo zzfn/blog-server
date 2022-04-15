cbb5f5be6670c16e83488c29d9bcfb9fdddf5a7d
chenc <44751363+zzfn@users.noreply.github.com>
2022-04-15 16:27:43 +0800

    feat: file

--- src/main/java/com/zzf/controller/FileController.java
+++ src/main/java/com/zzf/controller/FileController.java
@@ -1,20 +1,23 @@
 package com.zzf.controller;
 
+import com.aliyun.oss.ClientException;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
-import com.aliyun.oss.model.ListObjectsRequest;
-import com.aliyun.oss.model.PutObjectRequest;
-import com.zzf.util.RedisUtil;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.*;
 import com.zzf.util.ResultUtil;
 import com.zzf.vo.RequestVO;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -35,28 +38,83 @@ public class FileController {
     private String bucketName;
 
     @GetMapping("listFiles")
-    public Object listFiles(String path) {
+    public Object listFiles(String prefix) {
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
         ListObjectsRequest listObjectsRequest = new ListObjectsRequest(bucketName);
         listObjectsRequest.setDelimiter("/");
-        listObjectsRequest.setPrefix(path);
+        listObjectsRequest.setPrefix(prefix);
         return ResultUtil.success(ossClient.listObjects(listObjectsRequest));
     }
 
     @PostMapping("upload")
-    public Object upload(@RequestBody MultipartFile file,@RequestParam String path) throws IOException {
+    public Object upload(@RequestBody MultipartFile file, @RequestParam String path) throws IOException {
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
         PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, path + file.getOriginalFilename(), file.getInputStream());
         ossClient.putObject(putObjectRequest);
         ossClient.shutdown();
-        return null;
+        return ResultUtil.success(null);
     }
 
     @DeleteMapping("deleteFile")
     public Object deleteFile(@RequestBody RequestVO requestVo) {
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-        ossClient.deleteObject(bucketName,requestVo.getId());
+        ossClient.deleteObject(bucketName, requestVo.getId());
         ossClient.shutdown();
-        return null;
+        return ResultUtil.success(null);
+    }
+
+    @DeleteMapping("deleteFolder")
+    public Object deleteFolder(@RequestBody RequestVO requestVo) {
+        String prefix = requestVo.getId();
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            String nextMarker = null;
+            ObjectListing objectListing = null;
+            do {
+                ListObjectsRequest listObjectsRequest = new ListObjectsRequest(bucketName)
+                        .withPrefix(prefix)
+                        .withMarker(nextMarker);
+
+                objectListing = ossClient.listObjects(listObjectsRequest);
+                if (objectListing.getObjectSummaries().size() > 0) {
+                    List<String> keys = new ArrayList<String>();
+                    for (OSSObjectSummary s : objectListing.getObjectSummaries()) {
+                        System.out.println("key name: " + s.getKey());
+                        keys.add(s.getKey());
+                    }
+                    DeleteObjectsRequest deleteObjectsRequest = new DeleteObjectsRequest(bucketName).withKeys(keys).withEncodingType("url");
+                    DeleteObjectsResult deleteObjectsResult = ossClient.deleteObjects(deleteObjectsRequest);
+                    List<String> deletedObjects = deleteObjectsResult.getDeletedObjects();
+                    try {
+                        for (String obj : deletedObjects) {
+                            String deleteObj = URLDecoder.decode(obj, "UTF-8");
+                            System.out.println(deleteObj);
+                        }
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                nextMarker = objectListing.getNextMarker();
+            } while (objectListing.isTruncated());
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+        return ResultUtil.success(null);
     }
 }
