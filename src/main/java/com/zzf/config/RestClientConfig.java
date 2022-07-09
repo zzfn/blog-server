d0b6482ca17bf59ca20747e8dc1bd25372a162bf
cc <44751363+zzfn@users.noreply.github.com>
2022-07-09 19:34:43 +0800

    fix: es超时

--- src/main/java/com/zzf/config/RestClientConfig.java
+++ src/main/java/com/zzf/config/RestClientConfig.java
@@ -7,8 +7,6 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 
