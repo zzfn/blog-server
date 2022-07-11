93d44ce0372b2eb54073af78089396901e432df7
cc <44751363+zzfn@users.noreply.github.com>
2022-07-11 22:01:11 +0800

    fix: open-ui

--- src/main/java/com/zzf/config/SpringDocConfig.java
+++ src/main/java/com/zzf/config/SpringDocConfig.java
@@ -1,5 +1,12 @@
 package com.zzf.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -7,4 +14,17 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class SpringDocConfig {
+    final String securitySchemeName = "bearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .addServersItem(new Server().url("https://api.zzfzzf.com"))
+                .components(new Components().addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+                .info(new Info().title("个人博客API文档").version("V0"));
+    }
 }
