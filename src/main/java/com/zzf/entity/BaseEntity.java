cdafc3a1b41ab16f2193317912b97a248b9d86c7
cc <44751363+zzfn@users.noreply.github.com>
2022-07-17 11:30:31 +0800

    feat: bug

--- src/main/java/com/zzf/entity/BaseEntity.java
+++ src/main/java/com/zzf/entity/BaseEntity.java
@@ -25,11 +25,9 @@ public class BaseEntity implements Serializable {
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private String updateBy;
 
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     @TableField(fill = FieldFill.INSERT,jdbcType= JdbcType.TIMESTAMP)
     private LocalDateTime createTime;
 
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     @TableField(fill = FieldFill.INSERT_UPDATE,jdbcType= JdbcType.TIMESTAMP,update = "now()")
     private LocalDateTime updateTime;
 
